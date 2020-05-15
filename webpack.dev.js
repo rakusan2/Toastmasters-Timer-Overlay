d9396700367008831e6dd0fe22d354a2846e135c
TomasR <Rakusan2@gmail.com>
2020-05-15 18:48:43 -0400

    Add LiveReload dependency

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,11 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+var LiveReloadPlugin = require('webpack-livereload-plugin');
 
 module.exports = merge(common, {
   mode: 'development',
-  devtool: 'inline-source-map'
+  devtool: 'inline-source-map',
+  plugins: [
+    new LiveReloadPlugin({ appendScriptTag: true })
+  ]
 });
