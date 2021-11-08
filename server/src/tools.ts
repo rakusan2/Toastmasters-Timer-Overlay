53467a1c951b740018a54f5cce72797eea24fed0
Tomas Rakusan <10575235+rakusan2@users.noreply.github.com>
2021-11-08 15:17:11 -0700

    opne OBS with spawn

--- server/src/tools.ts
+++ server/src/tools.ts
@@ -2,50 +2,60 @@ import { platform } from 'os'
 
 const obsPath: { [key: string]: { path: string, cwd: string } | string } = {
     win32: { path: 'obs64.exe', cwd: 'C:/Program Files/obs-studio/bin/64bit' },
-    linux: 'obs', //TODO Change to correct location
+    linux: 'obs',
     darwin: '/Applications/OBS.app/Contents/MacOS/OBS'
 }
 
-export async function openOBS({ path, profile, scene, min, cwd }: { path?: string | boolean, profile?: string, scene?: string, min?: boolean, cwd?: string } = {}) {
+export async function openOBS({ path, profile, scene, min, cwd, args = [] }: { path?: string | boolean, profile?: string, scene?: string, min?: boolean, cwd?: string, args?: string[] } = {}) {
     const os = platform()
     if (path === false) return
     if (path === true || path === '') path = undefined
-    
+
     const temp = obsPath[os]
-    
-    if (typeof temp === 'string') path = path ?? temp
-    else {
+    if (temp == null) console.warn(`The platform ${os} does not have a default path`)
+
+    if (typeof temp === 'string') {
+        path = path ?? temp
+    } else if (temp != null) {
         path = path ?? temp.path
         cwd = cwd ?? temp.cwd
     }
 
-    if (path == null) throw new Error(`The platform ${os} does not have a default path`)
+    if ((typeof path !== 'string') || path.length === 0) throw new Error('OBS Path is undefiled')
 
-    let command = path + ' --startvirtualcam'
+    args.push('--startvirtualcam', '--disable-updater')
 
     if (profile != null && profile.length > 0) {
-        if (profile.includes('"')) throw new Error('profile has "')
-        command += ` --profile "${profile}"`
+        args.push('--profile', profile)
     }
     if (scene != null && scene.length > 0) {
-        if (scene.includes('"')) throw new Error('scene has "')
-        command += ` --scene "${scene}"`
+        args.push('--scene', scene)
     }
     if (min === true) {
-        command += ` --minimize-to-tray`
+        args.push(`--minimize-to-tray`)
     }
 
-    return await cmd(command, cwd)
+    return await cmd(path, args, cwd)
 }
 
-export function cmd(command: string, cwd?: string) {
-    return new Promise<string>((res, rej) => {
+export function cmd(command: string, args?: string[], cwd?: string): Promise<string>
+export function cmd(command: string, args: string[], cwd: string, callback: (data: string) => any): Promise<number>
+export function cmd(command: string, args?: string[], cwd?: string, callback?: (data: string) => any): Promise<string | number> {
+    return new Promise((res, rej) => {
         import('child_process').then(cp => {
+            let result = ''
             console.log('Executing > ' + command)
-            cp.exec(command, { windowsHide: true, cwd }, (err, data) => {
-                if (err != null) {
-                    rej(err)
-                } else res(data)
+            const ps = cp.spawn(command, args, { cwd })
+            ps.on('message', data => {
+                const str = data.toString()
+                if (callback != null) callback(str)
+                else result += str
+            })
+            ps.on('error', rej)
+            ps.on('close', code => {
+                if (code == null) code = 0
+                if (callback != null) res(code)
+                else res(result)
             })
         }).catch(rej)
     })
@@ -53,11 +63,7 @@ export function cmd(command: string, cwd?: string) {
 
 export async function open(address: string, opt?: import('open').Options) {
     const open = await import('open')
-    if (opt != null) {
-        await open(address, opt)
-    } else {
-        await open(address)
-    }
+    await open(address, opt)
     console.log(`Opened '${address}'`)
 }
 
@@ -65,7 +71,7 @@ const URL_BASE64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567
 export function getRandomStr64(len: number) {
     let res = ''
     for (let i = 0; i < len; i++) {
-        const index = Math.round(Math.random() * (URL_BASE64.length - 1))
+        const index = Math.floor(Math.random() * URL_BASE64.length)
         res += URL_BASE64[index]
     }
     return res
